71dcd4951ec0fb2e0665b4c0d8d43f49642823a6
Megan Tjuanta <megantjuanta@gmail.com>
2018-05-02 13:51:29 -0700

    started heap data structure. store names and scores of players and find winner (max heap)

---
+++ Heap.java
@@ -0,0 +1,52 @@
+import java.lang.*;
+import java.math.*;
+import java.io.*; 
+import java.util.*; 
+
+//HEAP MEANT TO SEARCH FOR WINNER OF TWSIFT GAME 
+
+
+public class Heap {//Heap class must be the same name as java file! 
+	int heap[] = 0;
+	int heap_size = 0; 
+
+public Heap() {
+	heap_size = 0; // default constructor
+}
+
+private void max_heap(int[] input) {
+	heap = new int[input.length+1];//store in heap starting from index 1 to avoid off by one error
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
