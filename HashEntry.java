eb8b3e5f5ef0fe300e7b8b5e2e3a95c60b340cf0
Megan Tjuanta <megantjuanta@gmail.com>
2018-05-10 18:15:59 -0700

    added another constructor to HashEntry

--- HashEntry.java
+++ HashEntry.java
@@ -5,9 +5,19 @@ class HashEntry {
 	int value;
 	HashEntry next;
 
+
+	HashEntry () {//default constructor 
+
+	key = ""; 
+	value = 0; 
+	next = null; 
+
+
+	}
+
 	HashEntry(String key, int value) {// constructor 
-		this.key = key; 
-		this.value = value;
+		this.key = key;
+		this.value = value; 
 		this.next = null;
 	}
 }
